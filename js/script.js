1b874270c2e3bef08658b311af7ae446ee1ba78a
ayaan0009018-tech <ayaan0009018@gmail.com>
2026-06-24 22:00:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const searchInput =
+document.getElementById("searchInput");
+
+if(searchInput){
+
+searchInput.addEventListener("keyup", function(){
+
+let filter =
+searchInput.value.toLowerCase();
+
+let diseases =
+document.querySelectorAll(".disease-btn");
+
+diseases.forEach(function(item){
+
+let text =
+item.textContent.toLowerCase();
+
+if(text.includes(filter)){
+item.style.display = "block";
+}
+else{
+item.style.display = "none";
+}
+
+});
+
+});
+
+}
+function darkMode(){
+    document.body.classList.toggle("dark");
+}
+function calculateBMI(){
+
+let w =
+document.getElementById("weight").value;
+
+let h =
+document.getElementById("height").value/100;
+
+let bmi =
+w/(h*h);
+
+document.getElementById("result")
+.innerHTML =
+"Your BMI: " + bmi.toFixed(2);
+
+}
